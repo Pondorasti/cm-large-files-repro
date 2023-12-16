6f8c3c76cc1bc3668382f5023f2d4a6f3b52d281
Alexandru Turcanu <pondorasti@gmail.com>
2023-12-15 17:30:10 -0800

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import { DOCUMENT } from "@/documents"
+import { javascript } from "@codemirror/lang-javascript"
+import { EditorState } from "@codemirror/state"
+import { EditorView, lineNumbers } from "@codemirror/view"
+import { minimalSetup } from "codemirror"
+import { useEffect, useRef, useState } from "react"
+
+
+export default function Home() {
+  const editorViewRef = useRef()
+  const [editorView, setEditorView] = useState<EditorView>()
+
+  useEffect(() => {
+    const extensions = [
+      minimalSetup,
+      lineNumbers(),
+      javascript({ jsx: true, typescript: true }),
+      EditorState.readOnly.of(true),
+      EditorView.editable.of(false),
+    ]
+
+    const view = new EditorView({
+      doc: DOCUMENT,
+      extensions,
+      parent: editorViewRef.current,
+    })
+
+    setEditorView(view)
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      editorView?.destroy()
+    }
+  }, [editorView])
+
+  // @ts-expect-error
+  return <main ref={editorViewRef} className="" />
+}
